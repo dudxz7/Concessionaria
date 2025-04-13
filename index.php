9ca73ce1d08c937a07e9e048cfb0eb161d938a62
Dudxz7 <luizeduardo98r4@gmail.com>
2025-04-13 11:02:56 -0300

    otimization in navbar.php

--- index.php
+++ index.php
@@ -47,43 +47,9 @@ $capital = isset($capitais[$estado]) ? $capitais[$estado] : "Cidade - Estado";
     <!-- Tela inicial com a imagem de fundo -->
     <div class="hero-section">
         <header>
-            <nav class="navbar">
-                <div class="logo">
-                    <a href="index.php">
-                        <img src="img/logoofcbmw.png" alt="Logo BMW">
-                    </a>
-                    <a href="index.php" id="textlogo">BMW</a>
-                </div>
-
-                <div class="location">
-                    <img src="img/pin-de-localizacao.png" alt="Ícone de localização">
-                    <div class="location-text">
-                        <span>Pesquisando ofertas em</span>
-                        <u><strong id="user-location"><?php echo htmlspecialchars($capital); ?> e Região</strong></u>
-                    </div>
-                </div>
-
-                <div class="nav-icons">
-                    <a href="carrinho.php">
-                        <img src="img/heart.png" alt="Favoritos" class="heart-icon">
-                    </a>
-                    <div class="login">
-                        <?php if ($usuarioLogado): ?>
-                            <!-- Se o usuário estiver logado, mostra o nome -->
-                            <a href="<?php echo $linkPerfil; ?>">
-                                <img src="img/usercomcontorno.png" alt="Perfil">
-                            </a>
-                            <a href="<?php echo $linkPerfil; ?>"><span><?php echo htmlspecialchars($nomeUsuario); ?></span></a>
-                        <?php else: ?>
-                            <!-- Se não estiver logado, mostra o link para login -->
-                            <a href="login.html">
-                                <img src="img/usercomcontorno.png" alt="Login">
-                            </a>
-                            <a href="login.html"><span>Entrar</span></a>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            </nav>
+            <div>
+                <?php include 'php/navbar.php'; ?>
+            </div>
         </header>
 
         <div class="hero-content">
